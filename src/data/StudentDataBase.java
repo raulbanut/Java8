4e15f16292d5cab207c925a31b434b949f09878d
Banut Raul <banut.raul@yahoo.com>
2022-02-04 11:30:46 +0200

    Data for examples.

---
+++ src/data/StudentDataBase.java
@@ -0,0 +1,47 @@
+package data;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StudentDataBase {
+    public static List<Student> getAllStudents() {
+        Student studentOne = new Student("Adam",
+                2,
+                3.6,
+                "male",
+                Arrays.asList("swimming", "basketball", "volleyball"));
+
+        Student studentTwo = new Student("Jenny",
+                2,
+                3.8,
+                "female",
+                Arrays.asList("swimming", "gymnastics", "soccer"));
+
+        Student studentThree = new Student("Emily",
+                3,
+                4.0,
+                "female",
+                Arrays.asList("swimming", "gymnastics", "aerobics"));
+
+        Student studentFour = new Student("Dave",
+                3,
+                3.9,
+                "male",
+                Arrays.asList("swimming", "gymnastics", "soccer"));
+
+        Student studentFive = new Student("Sophia",
+                4,
+                3.5,
+                "female",
+                Arrays.asList("swimming", "dancing", "football"));
+
+        Student studentSix = new Student("James",
+                4,
+                3.9,
+                "male",
+                Arrays.asList("swimming", "basketball", "baseball"));
+
+        List<Student> students = Arrays.asList(studentOne, studentTwo, studentThree, studentFour, studentFive, studentSix);
+        return students;
+    }
+}
