d6b6b15d309d74c095f6ab9708ff7fd3bd313738
Banut Raul <banut.raul@yahoo.com>
2022-02-04 15:47:59 +0200

    Add Function Student example.

---
+++ src/functionalInterfaces/FunctionStudentExample.java
@@ -0,0 +1,28 @@
+package functionalInterfaces;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class FunctionStudentExample {
+    // How to return a map of Student with gpa
+    static Function<List<Student>, Map<String, Double>> studentFunction = (students -> {
+        Map<String, Double> studentGradeMap = new HashMap<>();
+
+        students.forEach(student -> {
+            if (PredicateStudentExample.predicateOne.test(student)) {
+                studentGradeMap.put(student.getName(), student.getGpa());
+            }
+        });
+
+        return studentGradeMap;
+    });
+
+    public static void main(String[] args) {
+        System.out.println(studentFunction.apply(StudentDataBase.getAllStudents()));
+    }
+}
