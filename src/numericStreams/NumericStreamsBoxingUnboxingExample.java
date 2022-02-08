dc74e19379516e4c091f06fb8b7a9eda12cb6630
Banut Raul <banut.raul@yahoo.com>
2022-02-08 17:55:55 +0200

    Add numeric streams examples.

---
+++ src/numericStreams/NumericStreamsBoxingUnboxingExample.java
@@ -0,0 +1,30 @@
+package numericStreams;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class NumericStreamsBoxingUnboxingExample {
+
+    public static List<Integer> boxing() {
+        return IntStream.rangeClosed(1, 10)
+                //int
+                .boxed()
+                //Integer
+                .collect(Collectors.toList());
+    }
+
+    public static int unBoxing(List<Integer> integerList) {
+        //wrapper to primitive
+        return integerList.stream()
+                //Wrapper Integer Values
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Boxing: " + boxing());
+        List<Integer> integerList = boxing();
+        System.out.println("Unboxing: " + unBoxing(integerList));
+    }
+}
